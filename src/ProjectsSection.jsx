606f0d3c161b83fb363136b1134dae4dfa93cf1b
Levan Lominashvili <119792426+levy-237@users.noreply.github.com>
2024-01-29 17:12:10 +0100

    commit

--- src/ProjectsSection.jsx
+++ src/ProjectsSection.jsx
@@ -8,7 +8,7 @@ export default function ProjectsSection() {
       <p>
         Simply click on an image to access its live demo. Hover over each image
         to reveal descriptions of the corresponding projects. As you scroll
-        down, you'll find live links and GitHub repository URLs.
+        down, you will find live links and GitHub repository URLs.
       </p>
       <Projects />
     </div>
